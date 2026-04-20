b6620c9c4a26a32554b63c627ae780cb4a14978a
Ferre07 <r1092398@student.thomasmore.be>
2026-04-20 14:32:20 +0200

    Maintenance klaar

--- src/main/java/tm/itbachelors/projectclassiccars/model/Maintenance.java
+++ src/main/java/tm/itbachelors/projectclassiccars/model/Maintenance.java
@@ -2,4 +2,56 @@
 package tm.itbachelors.projectclassiccars.model;
 
 public class Maintenance {
+    private String type;
+    private int duration;
+    private double hourlyRate;
+    private Staff responsible;
+
+    public Maintenance() {
+    }
+
+    public Maintenance(String type) {
+        this.type = type;
+    }
+
+    public Maintenance(String type, int duration) {
+        this.type = type;
+        this.duration = duration;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public Staff getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(Staff responsible) {
+        this.responsible = responsible;
+    }
+
+    public double getPrice(){
+        return hourlyRate * duration / 60;
+    }
 }
