2e459b463a36a17239666692ae8c3a0cebd1be60
Ferre07 <r1092398@student.thomasmore.be>
2026-04-20 13:36:03 +0200

    Initial commit - project start

---
+++ src/main/java/tm/itbachelors/projectclassiccars/model/Maintenance.java
@@ -0,0 +1,5 @@
+// Ferre Goovaerts r1092398
+package tm.itbachelors.projectclassiccars.model;
+
+public class Maintenance {
+}
---
+++ src/main/java/tm/itbachelors/projectclassiccars/model/Owner.java
@@ -0,0 +1,6 @@
+// Ferre Goovaerts r1092398
+
+package tm.itbachelors.projectclassiccars.model;
+
+public class Owner {
+}
